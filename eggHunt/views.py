d5727877aee92ce366b5f6609dce8dffbfd7da8b
Logan Christensen <chris.lgn.jae@gmail.com>
2020-01-25 06:07:06 -0700

    Added flag, changed scoreboard logic

--- eggHunt/views.py
+++ eggHunt/views.py
@@ -1,6 +1,7 @@
 from eggHunt import eggHunt
 from flask import escape, flash, Flask, render_template, redirect, request, session, url_for
 
+
 @eggHunt.route("/", methods=['GET', 'POST'])
 def hello():
     '''This is the main page of the Egg hunt. It has a cookie that gets changed on a POST request. Sending a second post request
@@ -16,12 +17,11 @@ def hello():
 
     if request.method == 'POST':
         if session['room_number'] == 1818:
-            flash(r"The first part of the code is eggFlag{who-made-this}")
+            flash(r"You've found the hardest flag! eggFlag{who-made-this}")
         else:
             error = "Who's on first"
             session['room_number'] = 1818
-
-    return render_template('hello.html', error=error)
+    return render_template('hello.html', error=error),  200, {'Extra Header': r'eggHunt{page-headers-leak-info}'}
 
 @eggHunt.route("/logout")
 def logout():
@@ -38,27 +38,35 @@ def scoreboard():
     '''The scoreboard page checks submitted flags against a dictionary of flags. If a correct flag is submitted, the counter goes up
     and the session stores the flag as being solved. Duplicates cannot be submitted to increase score'''
 
-    flags = {0:r"eggFlag{extra-points-before-breakfast}", 1:r"eggFlag{who-made-this}", 2:r"eggFlag{view-source}"}
+    flags = {0:r"eggFlag{extra-points-before-breakfast}", 1:r"eggFlag{who-made-this}", 2:r"eggFlag{view-source}", 3:r"eggHunt{page-headers-leak-info}"}
     
     if 'challenges' not in session:
         session['challenges'] = 0
         session.modified = True
     if 'flags' not in session:
-        session['flags'] = {0 : False, 1 : False, 2 : False}
+        session['flags'] = {0 : False, 1 : False, 2 : False, 3 : False}
     
     if request.method == 'POST':
         flag = escape(request.form.get('Flag'))
+
         for i in flags:
             if flag == flags[i]:
                 if session['flags'][str(i)]:
                     flash(r"Already Submitted this flag")
-                
+                    break
                 else:
                     flash(r"Congrats! You've entered a correct flag")
                     session['challenges'] += 1
                     session['flags'][str(i)] = True
                     session.modified = True
+                    break
 
+        if not flag or flag == "":
+            flash(r"Please enter a flag")
+
+        if flag not in flags:
+            flash(r"Incorrect flag")
+        
     challenges = session['challenges']
 
     return render_template('scoreboard.html', challenges=challenges)
